cd93b52735c7c1ba139b163a7d98fe400ccc432c
daiken9471 <daiken9471@gmail.com>
2026-02-25 11:05:10 -0600

    Add files via upload

---
+++ me.js
@@ -0,0 +1,16 @@
+//David Aiken All about me 2/25/26
+
+//vaiable declarations
+let myName = "David";
+let favColor = "Orange";
+let favHobby = "gaming";
+let favFood = "pizza";
+let favAnimal = "dog";
+
+//display infomation
+console.log("My name is:", myName);
+console.log("MY favorite color is:", favColor);
+console.log("My favorte hobby is:", favHobby);
+console.log("My favorite food is:", favFood);
+console.log("My favorite animal is:", favAnimal);
+
