db3ab97f5a87dc9e6516a4b003e1949d0bb6beb9
daiken9471 <daiken9471@gmail.com>
2026-04-01 15:06:49 -0500

    Add files via upload

--- loops/spaceStation.js
+++ loops/spaceStation.js
@@ -1,21 +1,35 @@
 // David Aiken  4/1/26
 "use strict"
 
-let crates;
+let fragile = 0
+let inspected = 0
+let loaded = 0
+
+let crates
 do{
     crates = prompt(`How many crates are you loading?: `)?.trim()}
 while (crates === "" || crates === null)
 
 if (crates === undefined){
-    console.log(`Scan cancelled`)
-}else
-
+    alert(`Scan cancelled`)}
+   
 for (let crateNum = 1; crateNum <= crates; crateNum++){
     if (crateNum % 5 === 0){
-        console.log(`Handle with care`)}
+        console.log(`Handle with care while loading`)
+        fragile += 1}
     else if (crateNum % 2 === 0){
-        console.log(`Load crate`)}
+        console.log(`Load crate`)
+        loaded += 1}
     else if(crateNum % 2 === 1){
-        console.log(`Inspect crate`)}
-}
+        console.log(`Inspect crate before loading`)
+        inspected += 1}}
+
+if (crates !== undefined)
+    alert(`All crates scanned!
+
+Fragile crates : ${fragile}
+Inspected crates: ${inspected}
+Loaded normally: ${loaded}`)
+
+
 
